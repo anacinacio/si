dfbb5ea041e511f7d595fd302c13fae8eeb5e020
anacinacio <100290342+anacinacio@users.noreply.github.com>
2022-09-26 12:03:38 +0100

    Adding my co-authorship to README.md file

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "Ana Inacio"
-__credits__ = ["Vítor Pereira, Fernando Cruz,Ana Inacio"]
+__credits__ = ["Vítor Pereira, Fernando Cruz, Ana Inacio"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
 __maintainer__ = "Ana Inacio"
