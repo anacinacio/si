82dbaac9d22971a8e531313ddfebc8ca70d8fc76
anacinacio <100290342+anacinacio@users.noreply.github.com>
2023-01-01 20:59:07 +0000

    def euclidean_distance

--- src/si/statistics/euclidean_distance.py
+++ src/si/statistics/euclidean_distance.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def euclidean_distance(x,y):
+def euclidean_distance(x: np.ndarray, y: np.ndarray) -> np.ndarray:
     """
     Calcula a distância euclidiana entre X e Y usando a seguinte formula:
         distance_y1n = np.sqrt((x1 - y11)^2 + (x2 - y12)^2 + ... + (xn - y1n)^2)
@@ -15,5 +15,5 @@ def euclidean_distance(x,y):
         array com distância entre X e as várias amostras de Y
     """
 
-    return np.sqrt((x-y) ** 2).sum(axis=1)
+    return np.sqrt(((x - y) ** 2).sum(axis=1))
 
